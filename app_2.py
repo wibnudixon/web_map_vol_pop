04aae9f6f2f1512ac16e7a825b3a7319564020e6
Wibnu Dickson <wibnudickson@gmail.com>
2021-08-11 14:51:35 +0200

    changing the markers from markers to circles using circlemaker

--- app_2.py
+++ app_2.py
@@ -5,6 +5,7 @@ data = pandas.read_csv("volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
+
 def color_producer(elevation):
     if elevation < 1000:
        return 'green'
@@ -12,15 +13,12 @@ def color_producer(elevation):
         return 'orange'
     else:
         return 'red'
-
-
 map = folium.Map(location=[38.58,-99.09],zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, el in zip(lat,lon,elev):
-    print(type(el))
-    fg.add_child(folium.Marker(location=[lt,ln],popup=str(el)+"m",icon=folium.Icon(color=color_producer(el))))
+    fg.add_child(folium.CircleMarker(location=[lt,ln],radius= 6, popup=str(el)+"m",fill_color=color_producer(el), color='grey',fill_opacity=0.7))
 
 map.add_child(fg)
 
